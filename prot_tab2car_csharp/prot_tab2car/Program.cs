ec71a1d6bcfb5573891e97b86e8ea511aa75208d
Leon Colijn <lcolijn@xs4all.nl>
2012-05-14 21:46:28 +0200

    Add example C# pointer (de)referencing

---
+++ prot_tab2car_csharp/prot_tab2car/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+
+using System.Runtime.InteropServices;
+
+
+enum Tab2CarSteerMode
+{
+        XY,
+        Throttle
+};
+
+[StructLayout(LayoutKind.Sequential, Pack=1)]
+struct Tab2CarSteer
+{
+        byte x;
+        byte y;
+};
+
+
+[StructLayout(LayoutKind.Sequential, Pack = 1)]
+struct Tab2CarMotor
+{
+        UInt16 Throttle; // Promille
+        UInt16 Voltage; // Millivolts
+        UInt16 Current; // Milliamperes
+        UInt16 Temperature; // Millidegrees
+};
+
+[StructLayout(LayoutKind.Sequential, Pack=1)]
+struct Tab2CarPacket
+{
+        public Byte Command;
+        public Byte SteerMode;
+        public Tab2CarSteer Steer;
+        public Tab2CarMotor M0;
+        public Tab2CarMotor M1;
+        public Tab2CarMotor M2;
+        public Tab2CarMotor M3;
+}
+
+namespace prot_tab2car
+{
+        static class Program
+        {
+                /// <summary>
+                /// The main entry point for the application.
+                /// </summary>
+                [STAThread]
+                static void Main()
+                {
+                        unsafe {
+                                byte[] buffer = new byte[sizeof(Tab2CarPacket)];
+                                Tab2CarPacket* pack = null;
+
+                                // Vertel de Garbage Collector dat hij zijn tengels van onze buffer af moet houden:
+                                GCHandle gch = GCHandle.Alloc(buffer);
+                                pack = (Tab2CarPacket*)gch.AddrOfPinnedObject().ToPointer();
+
+                                // Doe je ding met de variabelen
+                                pack->SteerMode = (byte)Tab2CarSteerMode.Throttle;
+
+                                // En uiteindelijk:
+                                gch.Free();
+
+                                // Alles in de buffer is nu gewijzigd naar wat jij wil.
+                        }
+                }
+        }
+}
