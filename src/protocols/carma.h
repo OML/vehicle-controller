370a0fda100c765e9fff5b45c3f16770f5501288
Leon <lcolijn@xs4all.nl>
2012-05-15 15:38:51 +0200

    Implemented carma `sync' and `list_names' structures

--- src/protocols/carma.h
+++ src/protocols/carma.h
@@ -23,24 +23,48 @@
 
 #include <cstdint>
 
-struct tab2car_motor
+struct carma_motor
 {
         int16_t         throttle;       // promille
         uint16_t        voltage;        // millivolts
         uint16_t        current;        // milliamperes
-        uint16_t        temperature;    // tenth degrees
+        int16_t         temperature;    // tenth degrees
 } __attribute__((packed));
 
-
-struct tab2car_packet
+enum
 {
-        uint8_t turbo;
-        uint8_t calibrate;
-        int8_t motors[4];
-        int8_t left;
-        int8_t right;
-        uint8_t sound;
+        COP_SYNC = 1,
+        COP_LIST_NAMES,
+};
+
+struct carma_t2c_packet
+{
+        uint8_t opcode;
+        union {
+                struct {
+                        uint8_t speed;
+                        int8_t left;
+                        int8_t right;
+                        uint8_t calibrate;
+                        int8_t motors[4];
+                        uint8_t sound;
+                } sync;
+                struct {
+
+                } list_names;
+        };
 }__attribute__((packed));
+
+struct carma_c2t_packet
+{
+         uint8_t opcode;
+         struct {
+                carma_motor motors[4];
+                int16_t gyro[3];
+                int16_t accu_voltage;
+                int16_t accu_current;
+         } sync;
+};
 
 class carma: public protocol
 {
